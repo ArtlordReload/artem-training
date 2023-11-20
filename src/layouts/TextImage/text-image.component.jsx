0debc2f981c8a98b1aae779c0420ce94baf3543b
Artiom <adiakov@espe.edu.ec>
2023-11-19 19:16:43 -0500

    added TextImage

--- src/layouts/TextImage/text-image.component.jsx
+++ src/layouts/TextImage/text-image.component.jsx
@@ -1,14 +1,41 @@
 import React from "react"
 import * as S from "./text-image.styles.jsx"
 import PropTypes from "prop-types"
+import { StaticQuery } from "gatsby"
+import { Container } from "@mui/system"
+import Grid from "@mui/material/Grid"
 
-const TextImage = ({ description, image }) => {
+import { graphql, useStaticQuery } from "gatsby"
+
+const TextImage = () => {
+  const staticQuery = useStaticQuery(graphql`
+    query {
+      textImage: file(relativePath: { eq: "text-image.png" }) {
+        childImageSharp {
+          gatsbyImageData(layout: FULL_WIDTH)
+        }
+      }
+    }
+  `)
+
   return (
     <S.Wrapper>
-      <S.Title>Opening Day</S.Title>
-      <S.Descritpion>ASDadsasdf</S.Descritpion>
-      <S.Link href="https://www.google.com/">Shop Now</S.Link>
-      <S.Image />
+      <Container maxWidth="lg">
+        <Grid container spacing={8}>
+          <Grid xs={12} md={6} xl={6} item>
+            <S.LeftWrapper>
+              <S.Title>Opening Day</S.Title>
+              <S.Descritpion>Food Vendors, 1-2 brand cultivator popups</S.Descritpion>
+              <S.Link href="https://www.google.com/">Shop Now</S.Link>
+            </S.LeftWrapper>
+          </Grid>
+          <Grid xs={12} md={6} item>
+            <S.RightWrapper>
+              <S.Image img={staticQuery.textImage} />
+            </S.RightWrapper>
+          </Grid>
+        </Grid>
+      </Container>
     </S.Wrapper>
   )
 }
--- src/layouts/TextImage/text-image.styles.jsx
+++ src/layouts/TextImage/text-image.styles.jsx
@@ -2,39 +2,54 @@ import styled from "@emotion/styled"
 import CustomImage from "../../components/custom-image/custom-image.component.jsx"
 
 export const Wrapper = styled.div`
+  background-color: #f8fdf7;
+`
+
+export const LeftWrapper = styled.div`
   display: flex;
-  flex-direction: row;
-  border: 10px;
-  height: 100vh;
+  flex-direction: column;
+  align-items:center;
+  justify-content: flex-start;
+  position:absolute;
+  gap: 2rem;
+  padding-top: 10rem;
+  padding-left: 4rem;
+`
+
+export const RightWrapper = styled.div`
+  flex-direction: column;
 `
 
 export const Title = styled.h2`
   color: #00ae00;
-  font-size: 18px;
+  font-size: 64px;
   font-family: "Heebo";
   font-weight: 700;
   line-height: 26px;
+  align-self: flex-start;
 `
 
 export const Descritpion = styled.p`
-color: #f5f5f5;
-font-family: "Heebo";
-font-weight: 400;
-line-height: 26px;
+  color: #5F5F5F;
+  size: 16px;
+  font-family: "Heebo";
+  font-weight: 400;
+  line-height: 26px;
+  align-self: flex-start;
 `
 
 export const Link = styled.a`
   color: #00ae00;
-  font-size: 18px;
+  font-size: 14px;
   font-weight: 500;
   line-height: 12px;
-
+  align-self:flex-start;
 `
 
 export const Image = styled(CustomImage)`
   width: 100%;
   height: 100%;
-  min-width: 250px;
+  min-width: 722px;
   img {
     object-fit: contain !important;
   }
